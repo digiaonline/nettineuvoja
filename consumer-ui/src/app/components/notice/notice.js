e7bb1f2864068381dce2f2edb7ac476c0c77dc92
Christoffer Niska <christofferniska@gmail.com>
2016-03-29 16:02:49 +0300

    Fix bug that caused notices to not be translated correctly (fixes #11)

--- consumer-ui/src/app/components/notice/notice.js
+++ consumer-ui/src/app/components/notice/notice.js
@@ -2,12 +2,23 @@
 
 angular.module('nnConsumerUi')
 
+  .controller('NoticeController', function($scope) {
+    /**
+     * @param {object} item
+     * @returns {string}
+     */
+    $scope.translate = function(item) {
+      return languageService.translate(item, $scope.activeLanguage);
+    };
+  })
+
   .directive('nnNotice', function() {
     return {
       restrict: 'A',
       scope: {
         model: '=nnNotice'
       },
+      controller: 'NoticeController',
       templateUrl: 'components/notice/notice.html'
     };
   });
